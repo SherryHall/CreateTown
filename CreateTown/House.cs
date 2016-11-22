00fb1f2f7a280bff0ace9ba287b99a35b5a0d042
SherryHall <slhall222@gmail.com>
2016-11-22 00:07:31 -0500

    add classes for House, Bank, and School

---
+++ CreateTown/House.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTown
+{
+	class House
+	{
+		public int BathRooms;
+		public int BedRooms;
+		public int Floors;
+		public int NumberOfRoom;
+		public double Height;
+		public double Length;
+		public double Width;
+		public double SquareFootage = Length * Width;
+		public double Volume = SquareFootage * Height;
+
+		public string BuildingType;
+
+		public CurrentOccupants {get;};
+
+
+		CreateTown()
+	{
+		BuildingType = "House";
+	}
+
+	public int DecreaseOccupants()
+	{
+		return CurrentOccupants--;
+	}
+
+	public int IncreaseOccupants()
+	{
+		return CurrentOccupants++;
+	}
+	public override string DisplayName()
+	{
+		return this.Name;
+	}
+
+	public override string ToString()
+	{
+		return $"{BuildingType} : {Name}";
+	}
+}
+
